ad0e3e9f689501521e410e7d162f8afef155c26a
Beej Jorgensen <beej@beej.us>
2019-12-11 22:24:38 -0800

    tabs to spaces

--- examples/adv1.py
+++ examples/adv1.py
@@ -1,15 +1,15 @@
 # The map
 
 map_data = [
-	"#####################",
-	"#...#...............#",
-	"#...#..........#....#",
-	"#...#..........#....#",
-	"#...#..........#....#",
-	"#...#..........#....#",
-	"#..............#....#",
-	"#..............#....#",
-	"#####################"
+    "#####################",
+    "#...#...............#",
+    "#...#..........#....#",
+    "#...#..........#....#",
+    "#...#..........#....#",
+    "#...#..........#....#",
+    "#..............#....#",
+    "#..............#....#",
+    "#####################"
 ]
 
 # Player position
@@ -21,44 +21,44 @@ quit = False
 
 while not quit:
 
-	# Print map and player indicator
+    # Print map and player indicator
 
-	for row_index, row in enumerate(map_data):  # for each row
-		for col_index, map_character in enumerate(row):  # for each col
-			if row_index == player_row and col_index == player_column:
-				print("@", end="")  # end="" no newline
-			else:
-				print(map_character, end="")
-		print()
+    for row_index, row in enumerate(map_data):  # for each row
+        for col_index, map_character in enumerate(row):  # for each col
+            if row_index == player_row and col_index == player_column:
+                print("@", end="")  # end="" no newline
+            else:
+                print(map_character, end="")
+        print()
 
-	# Get input
+    # Get input
 
-	command = input("Enter a move (n,s,w,e,q): ")
+    command = input("Enter a move (n,s,w,e,q): ")
 
-	# Figure out the new row and column of the player
-	# Make sure input is valid
-	if command == "n":
-		new_row = player_row - 1
-		new_column = player_column
-	elif command == "s":
-		new_row = player_row + 1
-		new_column = player_column
-	elif command == "w":
-		new_row = player_row
-		new_column = player_column - 1
-	elif command == "e":
-		new_row = player_row
-		new_column = player_column + 1
-	elif command == "q":
-		quit = True
-		continue
-	else:
-		print(f'Unknown command {command}')
+    # Figure out the new row and column of the player
+    # Make sure input is valid
+    if command == "n":
+        new_row = player_row - 1
+        new_column = player_column
+    elif command == "s":
+        new_row = player_row + 1
+        new_column = player_column
+    elif command == "w":
+        new_row = player_row
+        new_column = player_column - 1
+    elif command == "e":
+        new_row = player_row
+        new_column = player_column + 1
+    elif command == "q":
+        quit = True
+        continue
+    else:
+        print(f'Unknown command {command}')
 
-	if map_data[new_row][new_column] == "#":
-		print("You can't move that way!")
-	else:
-		# Set the current position to the new position
-		player_row = new_row
-		player_column = new_column
+    if map_data[new_row][new_column] != ".":
+        print("You can't move that way!")
+    else:
+        # Set the current position to the new position
+        player_row = new_row
+        player_column = new_column
 
