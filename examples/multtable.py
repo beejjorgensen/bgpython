2736ad2f63d2eb4baed0d1d78ed8f7f2781d3f77
Brian "Beej Jorgensen" Hall <beej@beej.us>
2020-02-24 22:18:24 -0800

    tabs to spaces

--- examples/multtable.py
+++ examples/multtable.py
@@ -1,13 +1,13 @@
 valid_input = False
 
 while not valid_input:
-	x = input("Enter a number between 1 and 19: ")
-	x = int(x)
+    x = input("Enter a number between 1 and 19: ")
+    x = int(x)
 
-	if x >= 1 and x <= 19:
-		valid_input = True
-	
+    if x >= 1 and x <= 19:
+        valid_input = True
+
 for row in range(1, x + 1):
-	for product in range(row, row * (x + 1), row):
-		print(f"{product:4}", end="")
-	print()
+    for product in range(row, row * (x + 1), row):
+        print(f"{product:4}", end="")
+    print()
