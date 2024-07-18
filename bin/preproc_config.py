e46e50dc5e6c3fab3f0f8b0e3f6c2afd04ea66bd
Brian "Beej Jorgensen" Hall <beej@beej.us>
2024-07-18 11:58:49 -0700

    new build system

--- bin/preproc_config.py
+++
@@ -1,6 +0,0 @@
-EXAMPLE_URL = "https://beej.us/guide/bgpython/source/examples/"
-EXERCISE_URL = "https://beej.us/guide/bgpython/source/exercises/"
-WIKIPEDIA_URL = "https://en.wikipedia.org/wiki/"
-MAN_URL = "https://man.archlinux.org/man/"
-
-MANBREAK = "rule"
